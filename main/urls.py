d8a41f68daec935f163af934e5871eeb9e7be73c
Bautista Ribotta <bautistaribotta@hotmail.com>
2026-02-25 11:39:56 -0300

    Acomodo la estructura para el sistema de archivos que usa Django

---
+++ main/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from main.views import login, inicio
+
+urlpatterns = [
+    path('', login),
+    path('inicio', inicio)
+]
--- main/views.py
+++ main/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+def login(request):
+    return render(request, r"main/templates/login.html")
+
+def inicio(request):
+    return render(request, r"main/templates/inicio.html")
