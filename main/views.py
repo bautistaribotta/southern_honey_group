7b8499d12b58379f4ac8fc1fa15942612e0f712d
Bautista Ribotta <bautistaribotta@hotmail.com>
2026-02-28 12:23:09 -0300

    Agrego el rol del usuario a la vista de Inicio, ademas, uso un decorador para que se bloquee el acceso al inicio si el usuario no esta logueado

--- main/views.py
+++ main/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+
 def login(request):
     return render(request, "login.html")
 
-def inicio(request, ):
+"""
+Si un usuario anónimo quiere entrar a '/inicio/', bloqueo el acceso y
+lo envio de vuelta a '/' (pagina configurada del inicio del server) para que se loguee
+"""
+@login_required(login_url='/')
+def inicio(request):
     return render(request, "inicio.html")
