8d125554f87d886f9c11c98a5fc76e8952504e4b
Bautista Ribotta <bautistaribotta@hotmail.com>
2026-03-01 16:50:04 -0300

    Creo los archivos html y css para la estructura de mi aplicacion, ademas los conecto con la vista de inicio. Tambien agrego seguridad para que el usuario no pueda visitar los links internos sin autenticacion

--- main/urls.py
+++ main/urls.py
@@ -1,7 +1,15 @@
 from django.urls import path
-from main.views import login, inicio
+from main.views import login, inicio, productos, clientes, deudores, remitos
 
+"""
+La sentencia name="nombre_del_archivo" se usa 
+para que Django sepa la ruta relativa del .html
+"""
 urlpatterns = [
-    path('', login),
-    path('inicio/', inicio)
+    path('', login, name="login"),
+    path('inicio/', inicio, name="inicio"),
+    path('productos/', productos, name="productos"),
+    path('clientes/', clientes, name="clientes"),
+    path('deudores/', deudores, name="deudores"),
+    path('remitos/', remitos, name="remitos")
 ]
--- main/views.py
+++ main/views.py
@@ -5,10 +5,33 @@ from django.contrib.auth.decorators import login_required
 def login(request):
     return render(request, "login.html")
 
+
 """
-Si un usuario anónimo quiere entrar a '/inicio/', bloqueo el acceso y
-lo envio de vuelta a '/' (pagina configurada del inicio del server) para que se loguee
+Si un usuario anónimo (sin loguearse) quiere acceder a las paginas internas, 
+le bloqueo el acceso y lo envio de vuelta a '/' (pagina configurada del inicio del server) 
+para que se loguee. Todo esto implementado usando el wrapped @login_required
 """
+
+
 @login_required(login_url='/')
 def inicio(request):
     return render(request, "inicio.html")
+
+
+@login_required(login_url='/')
+def productos(request):
+    return render(request, "productos.html")
+
+
+@login_required(login_url='/')
+def clientes(request):
+    return render(request, "clientes.html")
+
+
+@login_required(login_url='/')
+def deudores(request):
+    return render(request, "deudores.html")
+
+@login_required(login_url='/')
+def remitos(request):
+    return render(request, "remitos.html")
