09cc534b099426450a9e18868cf265cd26a4c289
Bautista Ribotta <bautistaribotta@hotmail.com>
2026-03-14 16:21:51 -0300

    Corregido el bug que mostraba el input de edicion de cliente en el slider de nuevo cliente

---
+++ main/static/js/clientes.js
@@ -0,0 +1,15 @@
+const checkboxFactura = document.getElementById('factura');
+const campoCuit = document.getElementById('campo-cuit');
+
+if (checkboxFactura && campoCuit) {
+    function actualizarCuit() {
+        if (checkboxFactura.checked) {
+            campoCuit.style.display = 'flex';
+        } else {
+            campoCuit.style.display = 'none';
+        }
+    }
+    
+    checkboxFactura.addEventListener('change', actualizarCuit);
+    actualizarCuit(); 
+}
